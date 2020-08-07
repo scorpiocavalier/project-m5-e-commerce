54e68d4b703eb3242d7266b897ad6acd4045712f
Arturo Chen <art.can.fly@gmail.com>
2020-08-07 12:04:29 -0400

    removed test changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// I dont like STRICT
-// WHY WOULD YOU DO THIS?!
 ReactDOM.render(
     <App />,
   document.getElementById('root')
