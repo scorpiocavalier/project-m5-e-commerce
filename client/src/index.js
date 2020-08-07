83fcd6a47dc6aa49177c81045c0a961145888776
Arturo Chen <art.can.fly@gmail.com>
2020-08-07 11:27:02 -0400

    test conflict

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // I dont like STRICT
+// WHY WOULD YOU DO THIS?!
 ReactDOM.render(
     <App />,
   document.getElementById('root')
