433233b67bbc805b15a5986ff97bdce43c01e457
Arturo Chen <art.can.fly@gmail.com>
2020-08-09 04:58:02 -0400

    @Robbie: I left some suggestions in the files

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -4,18 +4,8 @@ import Product from "./Product";
 import testItems from "../Test Data/test_items";
 
 export default () => {
-  // const {
-  //   imageSrc,
-  //   name,
-  //   price,
-  //   numInStock,
-  //   category,
-  //   body_location,
-  //   companyId,
-  // } = testItems;
-
   return (
-    <div>
+    <>
       {testItems.map((item) => {
         return (
           // console.log("Product List Level Company", testItems.companyId);
@@ -31,6 +21,6 @@ export default () => {
           />
         );
       })}
-    </div>
+    </>
   );
-}
+};
