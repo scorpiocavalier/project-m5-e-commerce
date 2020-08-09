ffe138add0c650b17bd65ce53e25218c9f6b3a3e
kanwargill1992 <62477356+kanwargill1992@users.noreply.github.com>
2020-08-08 20:55:32 -0700

    components and product page (#10)
    * Headers,Homepage,ProductPage
    
    * removed extra companies and icloud  files
    
    * renamed items to test_items and moved to Test Data
    
    * added items.jsx & moved data to Test Data
    
    * changed to correct import location for companies
    
    * commit
    
    * removed duplicates
    
    * removed duplicates
    
    * working connections
    
    * components , product page
    
    Co-authored-by: Arturo Chen <art.can.fly@gmail.com>

---
+++ client/src/components/Pages/HomePage/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      All rights Reserved <span>&copy;</span>
+    </footer>
+  );
+};
+
+export default Footer;
