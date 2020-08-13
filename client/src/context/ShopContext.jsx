0265d5f7a7570eb8549fe3634ec83d4d5daded19
Arturo Chen <art.can.fly@gmail.com>
2020-08-12 23:25:04 -0400

    fetching items and companies from firebase works

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useReducer, useEffect } from "react";
 
 import { STATUS, setState } from "./actions";
 import { shopReducer } from "./reducers";
+import firebase from "../firebase/firebase";
 
 // Create the shop context and initial state
 const ShopContext = createContext();
@@ -19,28 +20,38 @@ export const useShopContext = () => useContext(ShopContext);
 export const ShopProvider = ({ children }) => {
   const [state, dispatch] = useReducer(shopReducer, initialState);
 
-  const fetchData = async () => {
-    try {
-      // Fetch the list of items from the API
-      let res = await fetch(`/products`);
-      const items = await res.json();
-      // Fetch the list of companies from the API
-      res = await fetch(`/companies`);
-      const companies = await res.json();
-      // Create a new state
-      const newState = {
-        status: STATUS.IDLE,
-        items,
-        companies
-      };
-      // Pass the new state to the dispatch
-      dispatch(setState(newState));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch the list of items from the API
+        // let res = await fetch(`/products`);
+        // const items = await res.json();
+        // Fetch the list of companies from the API
+        // res = await fetch(`/companies`);
+        // const companies = await res.json();
+
+        const database = firebase.database();
+        const itemsRef = database.ref('items');
+        const companiesRef = database.ref('companies');
+
+        const itemsSnapshot = await itemsRef.once('value');
+        const items = await itemsSnapshot.val() || null;
+
+        const companiesSnapshot = await companiesRef.once('value');
+        const companies = await companiesSnapshot.val() || null;
+
+        // Create a new state
+        const newState = {
+          status: STATUS.IDLE,
+          items,
+          companies,
+        };
+        // Pass the new state to the dispatch
+        dispatch(setState(newState));
+      } catch (error) {
+        console.log(error);
+      }
+    };
     fetchData();
   }, []);
 
